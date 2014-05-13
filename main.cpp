047dec5d856b602cb66b345b2027d59778ba4b6e
Jacek Jankowski <spock.sheldon@gmail.com>
2014-05-13 20:32:26 +0200

    Napierdolilem wiecej kodu

--- main.cpp
+++ main.cpp
@@ -23,6 +23,9 @@ char* window_name = "Edge Map";
 int min_adapt_max_val = 0;
 int min_adapt_block_size = 1;
 
+int canny_kernel_size = 1;
+int max_canny_kernel_size = 200;
+
 std::vector<cv::Vec4i> hough_lines;
 int hough_rho = 1;
 int hough_theta = 1;
@@ -32,6 +35,10 @@ int hough_max_line_gap = 10;
 
 vector<vector<Point> > contours;
 vector<Vec4i> hierarchy;
+
+vector<vector<Point> > approx_contours;
+
+struct
 
 void camera_raw_display(){
 	int c;
@@ -65,7 +72,8 @@ void canny_blur_filter(Mat &src, Mat &dst, int kernel_size, int low_treshold, in
 }
 
 void canny_window(int, void*){
-	canny_blur_filter(src_gray, dst, 3, canny_low_threshold, 3);
+	blur(src_gray, dst, Size(3,3));
+	Canny(dst, dst, 100, 100, 3);
 	imshow( window_name, dst );
 }
 
@@ -83,7 +91,7 @@ void adaptive_threshold_window(int, void*){
 
 void hough_canny_window(int, void*){
 	Mat temp;
-	canny_blur_filter(src_gray, temp, 3, 100, 3);
+	canny_blur_filter(src_gray, temp, 3, 50, 3);
 	HoughLinesP(temp, hough_lines, hough_rho, CV_PI/1800*hough_theta, hough_threshold,
 				(double)hough_min_line_length, (double)hough_max_line_gap);
 	temp.copyTo(dst);
@@ -97,20 +105,48 @@ void hough_canny_window(int, void*){
 
 void contours_window(int, void*){
 	Mat temp;
-	canny_blur_filter(src_gray, temp, 3, 100, 3);
+	canny_blur_filter(src_gray, temp, 3, 50, 3);
 	findContours( temp, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 	/// Draw contours
 	Mat drawing = Mat::zeros( temp.size(), CV_8UC3 );
-	for( int i = 0; i< contours.size(); i++ ){
-		Scalar color = Scalar( 255, 255, 255);
+	// Z tutka https://github.com/Itseez/opencv/blob/master/samples/cpp/contours2.cpp
+	approx_contours.resize(contours.size());
+	for(int i=0; i<contours.size(); i++){
+		approxPolyDP(Mat(contours[i]), approx_contours[i], 5, true);
+	}
+	// Plotujemy !!!!
+	for( unsigned int i=0; i< approx_contours.size(); i++ ){
+		Scalar color = Scalar( 255, 0, 0);
 		drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, Point() );
+		color = Scalar( 0, 255, 0);
+		drawContours( drawing, approx_contours, i, color, 1, 8, hierarchy, 0, Point() );
 	}
 	imshow(window_name, drawing);
 }
 
+void straighten(Mat &src, Mat &dst){
+	std::vector<cv::Vec4i> slines;
+	Mat temp;
+	blur(src, temp, Size(3,3));
+	Canny(temp, temp, 100, 100, 3);
+	HoughLinesP(temp, slines, 1, CV_PI/360, 65, 80, 10);
+	Mat drawing = Mat::zeros( temp.size(), CV_8UC3 );
+	drawing = Scalar::all(0);
+	for( unsigned int i = 0; i < slines.size(); i++ )
+	  {
+		Vec4i l = slines[i];
+		line( drawing, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+	  }
+	cout << slines.size();
+
+
+
+	imshow( window_name, drawing );
+}
+
 int main(int argc, const char** argv){
 	/// Load an image
-	src = imread( "/home/jacek/Studia/PIRO/face_rec_test/sheet.png" );
+	src = imread( "/home/jacek/Studia/PIRO/face_rec_test/benchmark.png" );
 	if( !src.data )
 	{ return -1; }
 	/// Convert the image to grayscale
@@ -121,8 +157,8 @@ int main(int argc, const char** argv){
 	/*
 	 * Trackbar Canny Threshold detector
 	 */
-	//createTrackbar("Min Threshold:", window_name, &canny_low_threshold, max_canny_low_threshold, canny_window);
-
+	createTrackbar("Min Threshold:", window_name, &canny_low_threshold, max_canny_low_threshold, canny_window);
+	//createTrackbar("Kernel size:", window_name, &canny_kernel_size, max_canny_kernel_size, canny_window);
 	/*
 	 * Trackbar Adaptive Threshold
 	 */
@@ -143,8 +179,9 @@ int main(int argc, const char** argv){
 	createTrackbar("min_line_length:", window_name, &hough_min_line_length, 200, hough_canny_window);
 	// 10 by tutorial
 	createTrackbar("max_line_gap:", window_name, &hough_max_line_gap, 200, hough_canny_window);
-	*/
 	createTrackbar("max_line_gap:", window_name, &hough_max_line_gap, 200, hough_canny_window);
-	contours_window(5, &min_adapt_block_size);
+	*/
+	// contours_window(5, &min_adapt_block_size);
+	straighten(src_gray, dst);
 	waitKey(0);
 }
